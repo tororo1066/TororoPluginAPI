0fa88358837aff85b53d01458f8c6fe4f7b82e52
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 23:22:56 +0900

    sTry追加

--- src/main/java/tororo1066/tororopluginapi/otherUtils/UsefulUtility.kt
+++ src/main/java/tororo1066/tororopluginapi/otherUtils/UsefulUtility.kt
@@ -1,10 +1,35 @@
 package tororo1066.tororopluginapi.otherUtils
 
-class UsefulUtility {
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+
+class UsefulUtility(val plugin: JavaPlugin) {
 
     companion object{
         fun doubleToFormatString(double: Double): String {
             return String.format("%,.0f",double)
         }
+
+        fun<R> sTry(unit: ()->R,onError: (Exception)->R) : R {
+            return try {
+                unit.invoke()
+            }catch (e : Exception){
+                onError.invoke(e)
+            }
+        }
     }
+
+    fun runTask(unit: ()->Unit){
+        Bukkit.getScheduler().runTask(plugin,unit)
+    }
+
+    fun doubleToFormatString(double: Double): String {
+        return UsefulUtility.doubleToFormatString(double)
+    }
+
+    fun<R> sTry(unit: ()->R,onError: (Exception)->R) : R {
+        return UsefulUtility.sTry(unit,onError)
+    }
+
+
 }
