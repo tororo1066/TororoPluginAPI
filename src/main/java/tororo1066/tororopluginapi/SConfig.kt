87617276749c206fbfc60adcae0e08aac6eeeb79
tororo_1066 <panshoku4@gmail.com>
2022-04-07 16:09:53 +0900

    SConfig.kt追加

---
+++ src/main/java/tororo1066/tororopluginapi/SConfig.kt
@@ -0,0 +1,33 @@
+package tororo1066.tororopluginapi
+
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
+class SConfig(val plugin: JavaPlugin) {
+
+    fun getConfig(path: String) : YamlConfiguration? {
+        val file = File(plugin.dataFolder.path + "/${path}.yml")
+        if (!file.exists())return null
+        return YamlConfiguration.loadConfiguration(file)
+    }
+
+    fun saveConfig(configuration: YamlConfiguration, path: String): Boolean {
+        val file = File(plugin.dataFolder.path + "/${path}.yml")
+        if (file.exists()){
+            configuration.save(file)
+            return true
+        }
+
+        val parent = file.parentFile
+        if (!parent.exists()){
+            if (!parent.mkdirs()) return false
+        }
+
+        if (!file.createNewFile()) return false
+
+        configuration.save(file)
+
+        return true
+    }
+}
