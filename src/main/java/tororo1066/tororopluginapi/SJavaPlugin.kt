6b9ca75f7f72c7f8cf345a9e25001339e75fda3e
tororo_1066 <panshoku4@gmail.com>
2022-04-22 16:36:22 +0900

    optionの追加方法変更

--- src/main/java/tororo1066/tororopluginapi/SJavaPlugin.kt
+++ src/main/java/tororo1066/tororopluginapi/SJavaPlugin.kt
@@ -1,25 +1,41 @@
 package tororo1066.tororopluginapi
 
 import org.bukkit.plugin.java.JavaPlugin
+import tororo1066.tororopluginapi.otherPlugin.SVault
 
 abstract class SJavaPlugin() : JavaPlugin() {
 
-    lateinit var sConfig: SConfig
-    lateinit var mysql: SMySQL
+    companion object {
+        lateinit var sConfig: SConfig
+        lateinit var mysql: SMySQL
+        lateinit var vault: SVault
+    }
 
     private var useMySQL = false
+    private var useVault = false
 
-    constructor(useMySQL: Boolean): this(){
-        this.useMySQL = useMySQL
+    private val useOptions = ArrayList<UseOption>()
+
+
+    constructor(vararg options: UseOption) : this(){
+        this.useOptions.addAll(options)
     }
 
 
     override fun onEnable() {
         saveDefaultConfig()
         sConfig = SConfig(this)
-        if (useMySQL){
+        if (useOptions.contains(UseOption.MySQL)){
             mysql = SMySQL(this)
         }
+        if (useOptions.contains(UseOption.Vault)){
+            vault = SVault()
+        }
+    }
+
+    enum class UseOption {
+        MySQL,
+        Vault
     }
 
 }
