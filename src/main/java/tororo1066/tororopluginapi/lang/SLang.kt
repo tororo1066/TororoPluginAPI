06f7b66b2af550426f314ffefacad292cd9b0e5d
tororo_1066 <panshoku4@gmail.com>
2022-05-10 20:25:57 +0900

    SLang test11

--- src/main/java/tororo1066/tororopluginapi/lang/SLang.kt
+++ src/main/java/tororo1066/tororopluginapi/lang/SLang.kt
@@ -29,8 +29,10 @@ class SLang(val plugin: JavaPlugin) {
         val file = File(plugin.dataFolder.path + "/LangFolder/")
         if (!file.exists()) file.mkdirs()
         val langList = plugin.getResource("/LangFolder/lang.txt")
+        Bukkit.broadcastMessage(langList.toString())
         langList?.bufferedReader()?.readLines()?.forEach {
-            plugin.saveResource("LangFolder/${it}.yml",false)
+            Bukkit.broadcastMessage(it)
+            plugin.saveResource("/LangFolder/${it}.yml",false)
         }
 
 
