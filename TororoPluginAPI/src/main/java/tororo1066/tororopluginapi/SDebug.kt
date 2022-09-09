a1a69570a5027b1d618b45bab4677fb36be7ecf2
tororo_1066 <panshoku4@gmail.com>
2022-09-10 08:19:06 +0900

    SDebug modify

--- TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/SDebug.kt
+++ TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/SDebug.kt
@@ -1,7 +1,9 @@
 package tororo1066.tororopluginapi
 
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import tororo1066.tororopluginapi.SDebug.Companion.sendDebug
 import java.util.UUID
 
 class SDebug {
@@ -19,6 +21,18 @@ class SDebug {
                 if (consoleSenderLevel >= level){
                     this.sendMessage(msg)
                 }
+            }
+        }
+
+        fun broadcastDebug(level: Int, msg: String){
+            Bukkit.getOnlinePlayers().forEach {
+                val pLevel = debugLevel[it.uniqueId]?:0
+                if (pLevel >= level){
+                    it.sendMessage(msg)
+                }
+            }
+            if (consoleSenderLevel >= level){
+                Bukkit.getConsoleSender().sendMessage(msg)
             }
         }
     }
