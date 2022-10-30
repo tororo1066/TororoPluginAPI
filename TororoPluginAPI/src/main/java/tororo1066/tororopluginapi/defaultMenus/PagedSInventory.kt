216908cc66dad52aafb14a6593b917ad18c0b009
tororo_1066 <panshoku4@gmail.com>
2022-10-31 03:04:28 +0900

    PagedSInventory.kt

---
+++ TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/defaultMenus/PagedSInventory.kt
@@ -0,0 +1,71 @@
+package tororo1066.tororopluginapi.defaultMenus
+
+import org.bukkit.Material
+import org.bukkit.plugin.java.JavaPlugin
+import tororo1066.tororopluginapi.sInventory.SInventory
+import tororo1066.tororopluginapi.sInventory.SInventoryItem
+import tororo1066.tororopluginapi.sItem.SItem
+
+class PagedSInventory(plugin: JavaPlugin,name: String,val row: Int): SInventory(plugin,name,row) {
+
+    private var items = ArrayList<HashMap<Int,SInventoryItem>>()
+
+    var nowPage = 0
+
+    private var leftSlots = listOf<Int>()
+    private var rightSlots = listOf<Int>()
+
+    fun setResourceItems(items: ArrayList<HashMap<Int,SInventoryItem>>) {
+        this.items = items
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun addPage(inv: SInventory){
+        val invItems = inv.javaClass.getField("items").get(inv) as HashMap<Int,SInventoryItem>
+        items.add(invItems)
+    }
+
+    fun addPage(items: HashMap<Int,SInventoryItem>){
+        this.items.add(items)
+    }
+
+    fun renderBar(){
+
+        val left = SItem(Material.RED_STAINED_GLASS_PANE).setDisplayName("§c§l前へ").toSInventoryItem().setCanClick(false).setClickEvent {
+            nowPage--
+            renderInventory(nowPage)
+        }
+
+        val right = SItem(Material.LIME_STAINED_GLASS_PANE).setDisplayName("§a§l次へ").toSInventoryItem().setCanClick(false).setClickEvent {
+            nowPage++
+            renderInventory(nowPage)
+        }
+
+        if (nowPage != 0){
+            leftSlots.forEach {
+                setItem(it,left)
+            }
+        }
+        if (nowPage+1 < items.size){
+            rightSlots.forEach {
+                setItem(it,right)
+            }
+        }
+
+    }
+
+    fun renderInventory(page: Int) {
+        clear()
+        renderBar()
+
+        for (i in 0 until row*9) {
+            setItem(i, this.items[page][i]?:continue)
+        }
+    }
+
+
+    override fun afterRenderMenu() {
+        renderInventory(0)
+    }
+
+}
