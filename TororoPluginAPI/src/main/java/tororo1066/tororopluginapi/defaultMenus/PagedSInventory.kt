50b2e0fed73db578443a698e19cdd96c521c9fa6
tororo_1066 <panshoku4@gmail.com>
2022-10-31 03:11:59 +0900

    PagedSInventory.kt 2

--- TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/defaultMenus/PagedSInventory.kt
+++ TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/defaultMenus/PagedSInventory.kt
@@ -6,7 +6,7 @@ import tororo1066.tororopluginapi.sInventory.SInventory
 import tororo1066.tororopluginapi.sInventory.SInventoryItem
 import tororo1066.tororopluginapi.sItem.SItem
 
-class PagedSInventory(plugin: JavaPlugin,name: String,val row: Int): SInventory(plugin,name,row) {
+open class PagedSInventory(plugin: JavaPlugin,name: String,val row: Int): SInventory(plugin,name,row) {
 
     private var items = ArrayList<HashMap<Int,SInventoryItem>>()
 
