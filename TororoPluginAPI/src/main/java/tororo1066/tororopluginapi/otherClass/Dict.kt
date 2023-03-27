f8593dddea4ea2069eb9a2b1dfd8d21cefc1aed7
tororo_1066 <panshoku4@gmail.com>
2023-03-27 23:01:28 +0900

    dict added

--- TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/otherClass/Dict.kt
+++ TororoPluginAPI/src/main/java/tororo1066/tororopluginapi/otherClass/Dict.kt
@@ -2,4 +2,13 @@ package tororo1066.tororopluginapi.otherClass
 
 open class Dict<V: Any>: HashMapPlus<String,V>() {
 
+    companion object{
+        fun<V: Any> dictOf(vararg pairs: Pair<String, V>): Dict<V> {
+            val dict = Dict<V>()
+            pairs.forEach {
+                dict[it.first] = it.second
+            }
+            return dict
+        }
+    }
 }
