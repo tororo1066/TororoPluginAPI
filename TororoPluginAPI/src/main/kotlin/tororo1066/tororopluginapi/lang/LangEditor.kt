c10e3260752e05f071dfbafe25507bb07df14c6d
tororo1066 <panshoku4@gmail.com>
2024-11-28 04:11:23 +0900

    LangEditor Support Color Code

--- TororoPluginAPI/src/main/kotlin/tororo1066/tororopluginapi/lang/LangEditor.kt
+++ TororoPluginAPI/src/main/kotlin/tororo1066/tororopluginapi/lang/LangEditor.kt
@@ -37,7 +37,7 @@ class LangEditor(plugin: JavaPlugin): LargeSInventory(plugin,"LangEditor") {
                         }
                         items.add(this.createInputItem(SItem(Material.EMERALD_BLOCK).setDisplayName("§aNew Insert"),String::class.java,"Please enter path.", invOpenCancel = true){ include, _ ->
                             SInput(plugin).sendInputCUI(p,String::class.java,"Please enter translated message.", action = { message ->
-                                yaml.set(include,message)
+                                yaml.set(include,message.replace("&","§"))
                                 yaml.save(File(plugin.dataFolder.path + "/LangFolder/${str}.yml"))
                                 this.open(p)
                             })
@@ -80,7 +80,7 @@ class LangEditor(plugin: JavaPlugin): LargeSInventory(plugin,"LangEditor") {
 
                     items.add(this.createInputItem(SItem(Material.EMERALD_BLOCK).setDisplayName("§aNew Insert"),String::class.java,"Please enter path.", invOpenCancel = true){ str, _ ->
                         SInput(plugin).sendInputCUI(p,String::class.java,"Please enter translated message.", action = {
-                            section.set("$path.$str",it)
+                            section.set("$path.$str",it.replace("&","§"))
                             yaml.save(File(plugin.dataFolder.path + "/LangFolder/${fileName}.yml"))
                             SLang.langFile[fileName] = yaml
                             this.open(p)
